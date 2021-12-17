ad65dc7eb79168e94c3120b456643e82e1f6f073
Pavankumarvinukonda <vinukondapavankumar1@gmail.com>
2021-12-17 11:26:42 +0000

    first commit

---
+++ src/components/Footer/index.js
@@ -0,0 +1,22 @@
+import {VscGithubAlt} from 'react-icons/vsc'
+import {FiInstagram} from 'react-icons/fi'
+import {FaTwitter} from 'react-icons/fa'
+import './index.css'
+
+const Footer = () => (
+  <div>
+    <h1 className="logo">
+      COVID19<span className="india">INDIA</span>
+    </h1>
+    <p className="tagline">
+      we stand with everyone fighting on the front lines
+    </p>
+    <div className="icon-container">
+      <VscGithubAlt className="icon" />
+      <FiInstagram className="icon" />
+      <FaTwitter className="icon" />
+    </div>
+  </div>
+)
+
+export default Footer
